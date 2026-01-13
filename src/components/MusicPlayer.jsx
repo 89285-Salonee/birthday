c533e835ab7311f4ed020535141658568d23e0be
89285-Salonee <shirsatsalonee510@gmail.com>
2026-01-14 01:12:33 +0530

    Fix GitHub Pages asset paths

--- src/components/MusicPlayer.jsx
+++ src/components/MusicPlayer.jsx
@@ -80,7 +80,7 @@ const MusicPlayer = forwardRef((props, ref) => {
 
   return (
     <>
-      <audio ref={audioRef} loop preload="auto">
+   <audio ref={audioRef} loop preload="auto">
   <source
     src={`${import.meta.env.BASE_URL}music.mp3`}
     type="audio/mpeg"
