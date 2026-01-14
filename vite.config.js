72b74fb22243104b189e0763c86ad9b6cf88f129
89285-Salonee <shirsatsalonee510@gmail.com>
2026-01-14 15:43:39 +0530

    Fix asset paths for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -19,12 +19,25 @@
 //   plugins: [react()],
 //   base: '/birthday/'
 // })
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react";
+
+// export default defineConfig({
+//   base: "/birthday/",
+//   plugins: [react()],
+// });
+
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   base: "/birthday/",
   plugins: [react()],
+  build: {
+    outDir: "docs",
+    emptyOutDir: true
+  }
 });
+
 
 
